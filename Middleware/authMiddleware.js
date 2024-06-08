cd3144266934e1a1f76b931fc4a3996fd074c1bb
merinpatel <patelmerin1212004@gmail.com>
2024-06-08 19:31:10 +0530

    first commit

---
+++ Middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+import JWT from 'jsonwebtoken'
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    console.log("hello")
+    const token = req.headers["authorization"].split(" ")[1];
+    JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        return res.status(200).send({
+          message: "Auth Fialed",
+          success: false,
+        });
+      } else {
+        console.log(decode.id)
+        req.body.userId = decode.id;
+        next();
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(401).send({
+      message: "Auth Failed",
+      success: false,
+    });
+  }
+};
+export default authMiddleware;
